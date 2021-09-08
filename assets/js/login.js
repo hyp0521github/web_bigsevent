7a402d46147349fa872d98097afd66e3b101f49c
inhyp <1647529823@qq.com>
2021-09-08 22:47:11 +0800

    新写了index文件和js

--- assets/js/login.js
+++ assets/js/login.js
@@ -31,7 +31,7 @@ $('#form_reg').on('submit', function (e) {
     e.preventDefault();
     const username = $('#name').val();
     const password = $('#password').val();
-    $.post('api/reguser',
+    $.post('/api/reguser',
         {
             username: username,
             password: password
@@ -52,7 +52,7 @@ $('#form_login').on('submit', function (e) {
     e.preventDefault();
     $.ajax({
         method: 'post',
-        url: 'api/login',
+        url: '/api/login',
         data: $(this).serialize(),
         success: function (res) {
             if (res.status !== 0) {
